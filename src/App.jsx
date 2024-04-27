e2335427b0278a2e7dad7871662ca3e390ed30be
asuru56 <56asuru@gmail.com>
2024-04-27 20:24:47 +0530

    Added Flip

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import './App.css';
+import SingleCards from './Components/SingleCards'; 
 
 const CardImages = [
-  { src: "/img/image1.jpeg" },
-  { src: "/img/image2.jpeg" },
-  { src: "/img/image3.jpeg" },
-  { src: "/img/image4.jpeg" },
-  { src: "/img/image5.jpeg" },
-  { src: "/img/image6.jpeg" },
-  { src: "/img/image7.jpeg" },
-  { src: "/img/image8.jpeg" },
+  { src: "/img/image1.jpeg", name:"richard-feynman",matched:false },
+  { src: "/img/image2.jpeg", name:"linus-toldavis" ,matched:false},
+  { src: "/img/image3.jpeg", name: "mozilla",matched:false },
+  { src: "/img/image4.jpeg", name:"richard-feynman",matched:false },
+  { src: "/img/image5.jpeg", name:"linus-toldavis",matched:false  },
+  { src: "/img/image6.jpeg", name: "mozilla",matched:false},
+  { src: "/img/image7.jpeg", name:"blender" ,matched:false},
+  { src: "/img/image8.jpeg", name:"blender",matched:false },
 ];
 
 function App() {
   const [cards, setCards] = useState([]);
   const [turns, setTurns] = useState(0);
+  const [choiceOne,setChoiceOne] = useState(null); 
+  const [choiceTwo,setChoiceTwo] = useState(null); 
 
   const shuffleCards = () => {
     const shuffledCards = [];
@@ -31,20 +34,57 @@ function App() {
     setTurns(0);
   };
 
+  const handleChoice = (card)=>{
+
+    choiceOne ? setChoiceTwo(card):setChoiceOne(card);
+    console.log("selected"+card.name);
+
+   
+
+  }
+
+  useEffect(()=>{
+    if(choiceOne && choiceTwo){
+      console.log("works here ");
+    if(choiceOne.name === choiceTwo.name)
+    {
+      setCards(prevCards=>{
+        return prevCards.map(card=>{
+          if(card.name === choiceOne.name){
+            return {...card,matched:true};
+          }
+          else{
+            return card; 
+          }
+        })
+      })
+    }
+    else{
+      console.log("Do not match"); 
+      resetTurn();
+    }
+  }
+
+  },[choiceOne,choiceTwo])
+
+  console.log(cards);
+
+  const resetTurn = () =>{
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setTurns(prevTurns => prevTurns+1)
+
+  }
+
   return (
     <div className="App">
       <h1>Open Source Matching</h1>
-      <p>Match the legendary founders with their creations and Remember it !!! </p>
+      <p> Have a nice day</p>
       <button onClick={shuffleCards}>New Game</button>
       <div className="card-grid">
-        {cards.map((card) => (
-          <div className="card" key={card.id}>
-            <div>
-              <img className="front" src={card.src} alt="card-front" height= "200px" width = "200px" />
-              <img className="back" src="src/assets/cover.png" alt="card back" />
-            </div>
-          </div>
-        ))}
+       {cards.map(card=>(
+            <SingleCards key= {card.id} card = {card} handleChoice= {handleChoice} flipped = {card == choiceOne || choiceTwo || card.matched} /> 
+       ))}
       </div>
     </div>
   );
---
+++ src/Components/SingleCards.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './SingleCards.css';
+
+
+ 
+function SingleCards({card , handleChoice, flipped }) {
+
+  const handleClick = ()=>{
+    handleChoice(card)
+      
+    }
+
+  return (
+  
+          <div className="card" key={card.id}>
+              <div className= {flipped ? "flipped" : "" }>
+              <img className="front" src={card.src} alt="card-front" height= "200px" width = "200px" />
+              <img className="back"
+               src="src/assets/cover.png" 
+               alt="card back" 
+               onClick = {handleClick} />
+               </div>
+       
+            </div>
+       
+  )
+}
+
+export default SingleCards; 
